1440e699fa29af889314133d5dc8f53a58313e91
Ritik6559 <ritikjoshi741@gmail.com>
2024-05-07 23:32:32 +0530

    adding title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData.dark(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       home: const WeatherScreen(city: "Mumbai",),
